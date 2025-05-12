02e6329c51695f45fff161fae8e570cb3f926011
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-05-12 14:37:35 +0700

    feat: add FeaturedPortofolio component with project details and links

---
+++ src/components/FeaturedPortofolio.svelte
@@ -0,0 +1,89 @@
+<script>
+</script>
+
+<section
+  id="projects"
+  class="mt-10 p-4 border border-gray-700 bg-green-100 retro-shadow"
+>
+  <h2 class="text-2xl text-green-900 font-bold mb-4">
+    📁 Featured Projects/Portofolio
+  </h2>
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div class="p-2 border-2 border-black bg-white">
+      <h3 class="font-bold">🖼️ Ageing Asia Website Revamp</h3>
+      <span class="italic">June 2024-Oct 2025</span>
+      <p class="mt-2">
+        Ageing Asia is an organization based in Singapore that focuses on
+        improving the quality of life for older adults in Asia by promoting
+        innovation, collaboration, and sustainable solutions in the aging
+        sector. Founded in 2009, Ageing Asia works as a platform that brings
+        together key stakeholders—such as governments, businesses, non-profits,
+        and researchers—to address the challenges and opportunities related to
+        aging populations across the Asia-Pacific region.
+      </p>
+      <a
+        href="https://ageingasia.com/"
+        target="_blank"
+        class="text-blue-800 underline hover:text-red-600">→ View Project</a
+      >
+    </div>
+    <div class="p-2 border-2 border-black bg-white">
+      <h3 class="font-bold">🖼️ World Ageing Festival 2025</h3>
+      <span class="italic">June 2024-Augustus 2025</span>
+      <p class="mt-2">
+        The World Ageing Festival is an international event that celebrates and
+        addresses issues related to aging populations worldwide. It typically
+        features a mix of presentations, discussions, exhibitions, and workshops
+        focused on various aspects of aging, including health, wellness,
+        technology, social impact, and policy. Attendees often include
+        professionals from healthcare, government, business, academia, and other
+        sectors, all aiming to share insights and innovations for improving the
+        quality of life for older adults.
+      </p>
+      <a
+        href="https://www.worldageingfestival.com/waf2025"
+        target="_blank"
+        class="text-blue-800 underline hover:text-red-600">→ View Project</a
+      >
+    </div>
+    <div class="p-2 border-2 border-black bg-white">
+      <h3 class="font-bold">🖼️ vue-zxing-scanner</h3>
+      <span class="italic">June 2021</span>
+      <p class="mt-2">ZXingjs Wrapper for Vue 2</p>
+      <a
+        href="https://github.com/trikarai/vue-zxing-scanner"
+        target="_blank"
+        class="text-blue-800 underline hover:text-red-600">→ View Project</a
+      >
+    </div>
+    <div class="p-2 border-2 border-black bg-white">
+      <h3 class="font-bold">🖼️ Innovation Management Platform</h3>
+      <span class="italic">September 2019-May 2021</span>
+      <p class="mt-2">
+        Innov.ID is an innovation management system. By leveraging this
+        solution, organization (enterprise, incubators, accelerators, etc) can
+        easily track progress and issues of each innovation innitiatives being
+        concurrently conducted by different innovator teams, and finally decide
+        the right support for each of them.
+      </p>
+      <a
+        href="https://innov.id/"
+        target="_blank"
+        class="text-blue-800 underline hover:text-red-600">→ View Project</a
+      >
+    </div>
+    <!-- More projects can go here -->
+  </div>
+  <hr class="mt-6 mb-6" />
+  <div class="flex w-full justify-center animate-bounce mt-3">
+    <a
+      href="mailto:mas.trisutrisno@gmail.com"
+      class=" text-blue-800 font-mono font-semibold"
+    >
+      Contact Me Full Full Access
+    </a>
+  </div>
+</section>
+
+<style>
+</style>
