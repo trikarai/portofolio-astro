5ddb9098a7586848086eb37960fa0ef4420a4368
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-05-12 14:34:34 +0700

    feat: add AboutMe component with introductory information and styling

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,18 @@
+<script setup>
+import IconVue from "./icon/IconVue.vue";
+</script>
+<template>
+  <section
+    id="about"
+    class="mt-10 p-4 border-2 border-dashed border-gray-500 bg-blue-50 retro-shadow relative"
+  >
+    <IconVue />
+    <h2 class="text-2xl text-blue-900 font-bold mb-2">🧑 About Me</h2>
+    <p>
+      Front-end developer with 5+ years of experience specializing in Vue.js and
+      React. Proven track record of successfully delivering projects using
+      modern UI frameworks like Vuetify, PrimeVue, and Tailwind CSS. Passionate
+      about building high-performance, user-friendly web applications.
+    </p>
+  </section>
+</template>
