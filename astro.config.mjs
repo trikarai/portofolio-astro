c8981d8b83d3e07516b098cf6a0315daab3be3a6
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-05-12 15:12:03 +0700

    feat: update .gitignore to include .netlify and remove unused node adapter import

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-import node from "@astrojs/node";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 
