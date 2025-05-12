fbd387178a43cc736a6806a5a2b820720a86cc4d
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-05-12 14:35:53 +0700

    feat: add retro icons for React and Vue components

---
+++ src/components/icon/IconReact.jsx
@@ -0,0 +1,12 @@
+import IconRetroReact from "../../assets/icon/icon-retro-react.png";
+
+export default function RetroNav() {
+  return (
+    <div
+      className="absolute right-1 top-1 cursor-help hover:scale-125"
+      title="React Component"
+    >
+      <img src={IconRetroReact.src} width="36" height="36" alt="React" />
+    </div>
+  );
+}
---
+++ src/components/icon/IconVue.vue
@@ -0,0 +1,12 @@
+<script setup>
+import IconRetroVue from "../../assets/icon/icon-retro-vue.png";
+</script>
+
+<template>
+  <div
+    class="absolute right-1 top-1 cursor-help hover:scale-125"
+    title="Vue Component"
+  >
+    <img :src="IconRetroVue.src" width="36" height="36" alt="Vue" />
+  </div>
+</template>
