9f5f59ec2cb94e2321d6bd9616e802eaed9b0978
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-05-12 14:33:54 +0700

    feat: update package.json with new dependencies and scripts; add svelte.config.js; enhance tsconfig.json for JSX support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+
+import node from "@astrojs/node";
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    vite: {
+  output: "server",
+  adapter: node({ mode: "standalone" }),
+  vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react(), vue(), svelte()],
 });
